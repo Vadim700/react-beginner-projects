ebaf9bfc333b5fa3d85b016b8ef6a2650cfb4c65
Vadim <bekmansurov700@mail.ru>
2023-06-12 23:21:35 +0500

    убрал ковычки

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './index.scss';
 function App() {
   const [count, setCount] = React.useState(0);
 
-  const onClickPlus = () => { setCount(count + 1); }
-  const onClickMinus = () => { setCount(count - 1); }
+  const onClickPlus = () => setCount(count + 1);
+  const onClickMinus = () => setCount(count - 1);
 
   return (
     <div className="App">
