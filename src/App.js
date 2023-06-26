0cc307dd720a17a5e06aec923ce1540ec27bd00a
Vadim <bekmansurov700@mail.ru>
2023-06-26 22:13:03 +0500

    123

--- src/App.js
+++ src/App.js
@@ -3,10 +3,50 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCarrency, setFromCarrency] = React.useState('RUB');
+  const [toCarrency, setToCarrency] = React.useState('USD');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
+
+  const [rates, setRates] = React.useState({});
+
+  React.useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/latest.js')
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.rates);
+        console.log(json.rates)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
+  const onChengeFromPrice = (value) => {
+    const price = value / rates[fromCarrency];
+    const result = price * rates[toCarrency];
+
+    console.log(price)
+    console.log(result)
+
+    setToPrice(result);
+    setFromPrice(value);
+  }
+  const onChengeToPrice = (value) => {
+    setToPrice(value);
+  }
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block
+        value={fromPrice}
+        currency={fromCarrency}
+        onChangeCurrency={setFromCarrency}
+        onChangeValue={onChengeFromPrice} />
+      <Block
+        value={toPrice}
+        currency={toCarrency}
+        onChangeCurrency={setToCarrency}
+        onChangeValue={onChengeToPrice} />
+
     </div>
   );
 }
